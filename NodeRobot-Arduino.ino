450e249d290f31fabac3f5b641be6d010a87a89f
dequevedo <danielelvisquevedo@gmail.com>
2019-09-10 20:59:29 -0300

    Access Point

--- NodeRobot-Arduino.ino
+++ NodeRobot-Arduino.ino
@@ -10,10 +10,12 @@
 #define right1 D0
 #define right2 D6
 
-const char *ssid = "Desktop_F0712324";
-const char *password = "16217053";
-int port = 8888;
-WiFiServer server(port);
+const char* ssid = "NodeRobot";
+const char* password = "noderobot";
+boolean alreadyConnected = false; // whether or not the client was connected previously
+
+WiFiServer server(80);
+
 
 void setup()
 {
@@ -27,68 +29,68 @@ void setup()
   pinMode(right1, OUTPUT);
   pinMode(right2, OUTPUT);
 
-  //pinMode(pino_led, OUTPUT);
-
-  //pinMode(SendKey, INPUT_PULLUP); //Btn to send data
   Serial.println();
+  delay(1000);
 
-  WiFi.mode(WIFI_STA);
-  WiFi.begin(ssid, password); //Connect to wifi
+  delay(1000);
 
-  Serial.println("Connecting to Wifi");
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
-    Serial.print(".");
-    delay(500);
-  }
+  Serial.println();
+  Serial.print("Configuring access point...");
+  /* You can remove the password parameter if you want the AP to be open. */
+  WiFi.softAP(ssid, password);
+  delay(1000);
+  IPAddress myIP = WiFi.softAPIP();
+  Serial.print("AP IP address: ");
+  Serial.println(myIP);
 
-  Serial.println("");
-  Serial.print("Connected to ");
-  Serial.println(ssid);
-
-  Serial.print("IP address: ");
-  Serial.println(WiFi.localIP());
+  Serial.println("\nStarting server...");
+  // start the server:
   server.begin();
-  Serial.print("Open Telnet and connect to IP:");
-  Serial.print(WiFi.localIP());
-  Serial.print(" on port ");
-  Serial.println(port);
 }
 
 void loop()
 {
   WiFiClient client = server.available();
 
-  if (client) {
-    if (client.connected())
+  if (client)
+  {
+    if (!alreadyConnected)
     {
-      Serial.println("Client Connected");
+      client.flush();
+      Serial.println("We have a new client");
+      //client.println("Hello, client!");
+      alreadyConnected = true;
     }
 
-    while (client.connected()) {
-      while (client.available() > 0) {
-        int leitura = client.read();
-
-        if (leitura == 69) {
-          int leSpd = client.read() * 8;
-          int x1 = client.read();
-          int x2 = client.read();
-
-          int y1 = client.read();
-          int y2 = client.read();
-          int riSpd = client.read() * 8;
-
-          RotateMotors(leSpd, x1, x2, y1, y2, riSpd);
-        }
-        client.flush();
-      }
+    if (client.available() > 0) {
+      // read the bytes incoming from the client:
+      char thisChar = client.read();
     }
-    client.stop();
-    Serial.println("Client disconnected");
-
-    analogWrite(left1, 0);
-    analogWrite(left2, 0);
-    analogWrite(right1, 0);
-    analogWrite(right2, 0);
   }
+
+  while (client.connected()) {
+    while (client.available() > 0) {
+      int leitura = client.read();
+
+      if (leitura == 69) {
+        int leSpd = client.read() * 8;
+        int x1 = client.read();
+        int x2 = client.read();
+
+        int y1 = client.read();
+        int y2 = client.read();
+        int riSpd = client.read() * 8;
+
+        RotateMotors(leSpd, x1, x2, y1, y2, riSpd);
+      }
+      client.flush();
+    }
+  }
+  client.stop();
+  Serial.println("Client disconnected");
+
+  analogWrite(left1, 0);
+  analogWrite(left2, 0);
+  analogWrite(right1, 0);
+  analogWrite(right2, 0);
 }
