25960481d86519b3734621bf91e22140268eb388
dequevedo <danielelvisquevedo@gmail.com>
2019-09-25 23:45:38 -0300

    a

--- NodeRobot-Arduino.ino
+++ NodeRobot-Arduino.ino
@@ -1,6 +1,13 @@
 #include <ESP8266WiFi.h>
 #include <WiFiClient.h>
 #include <ESP8266WebServer.h>
+//#include <Servo.h>
+//#include <Ultrasonic.h>
+
+/*#define pino_trigger 4
+  #define pino_echo 5*/
+
+#define pino_led 15
 
 #define leftSpeed D5
 #define left1 D4
@@ -10,16 +17,39 @@
 #define right1 D0
 #define right2 D6
 
+<<<<<<< HEAD
 const char* ssid = "NodeRobot";
 const char* password = "noderobot";
 boolean alreadyConnected = false; // whether or not the client was connected previously
 
 WiFiServer server(80);
 
+=======
+//Ultrasonic ultrasonic(pino_trigger, pino_echo);
+
+//Servo myservo;
+
+int actualPos = 95;
+int speed = 1;
+int manualSpeed = 5;
+int minPos = 50;
+int maxPos = 140;
+float cmMsec;
+
+const char *ssid = "Desktop_F0712324";
+const char *password = "16217053";
+int port = 8888;
+WiFiServer server(port);
+>>>>>>> parent of 3eddbe6... Code cleaning
+
+int count = 0;
 
 void setup()
 {
   Serial.begin(115200);
+  Serial.begin(9600);
+  /*myservo.attach(15);
+    myservo.write(actualPos);*/
 
   pinMode(leftSpeed, OUTPUT);
   pinMode(left1, OUTPUT);
@@ -47,6 +77,8 @@ void setup()
   // start the server:
   server.begin();
 }
+
+int num = 0;
 
 void loop()
 {
@@ -81,7 +113,28 @@ void loop()
         int y2 = client.read();
         int riSpd = client.read() * 8;
 
+<<<<<<< HEAD
         RotateMotors(leSpd, x1, x2, y1, y2, riSpd);
+=======
+          RotateMotors(leSpd, x1, x2, y1, y2, riSpd);
+        }
+
+        /*if (leitura == 1) {
+          int leitura2 = client.read();
+          RotateMotors(leitura2);
+          } else if (leitura == 2) {
+          int leitura2 = client.read();
+
+          if(leitura2 == 0){
+            digitalWrite(pino_led, LOW);
+            Serial.println("Desligou Farol");
+          }else{
+            digitalWrite(pino_led, HIGH);
+            Serial.println("Ligou Farol");
+          }
+          }*/
+        client.flush();
+>>>>>>> parent of 3eddbe6... Code cleaning
       }
       client.flush();
     }
